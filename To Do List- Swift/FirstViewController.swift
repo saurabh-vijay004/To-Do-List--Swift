d593bb2ec731a6e8a40bf122dfda1f6968c46197
saurabh-vijay004 <saurabh.vijay@iksulaops.com>
2018-05-21 18:43:22 +0530

    first commit

--- To Do List- Swift/FirstViewController.swift
+++ To Do List- Swift/FirstViewController.swift
@@ -8,17 +8,68 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    
+    
+    var items : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let taskObject = UserDefaults.standard.object(forKey: "tasks")
+        
+        if let tempTask = taskObject as? [String] {
+            
+            items = tempTask;
+            
+        }
+        
+        table.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+   
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row] 
+        
+        return cell;
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "tasks")
+            
+        }
+        
+    }
+    
+    
+    
 
 
 }
--- To Do List- Swift/SecondViewController.swift
+++ To Do List- Swift/SecondViewController.swift
@@ -8,8 +8,51 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var itemTextField: UITextField!
 
+    
+    @IBAction func addItem(_ sender: UIButton) {
+        
+        print(itemTextField.text ?? "nil")
+        
+        if !((itemTextField.text?.isEmpty ?? true ))! {
+            
+            let taskObject = UserDefaults.standard.object(forKey: "tasks");
+            
+            var task:[String]
+            
+            if let tempTask = taskObject as? [String] {
+                print(tempTask)
+                
+                task = tempTask
+                
+                task.append(itemTextField.text!)
+                
+            } else {
+                
+                task = [itemTextField.text!]
+            
+            }
+            
+            
+            UserDefaults.standard.set(task, forKey: "tasks");
+        
+//            print(task)
+            itemTextField.text = "";
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Alert", message: "Enter Task to Add", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+ 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +62,20 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
+        self.view.endEditing(true);
+
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
 
 }
 
